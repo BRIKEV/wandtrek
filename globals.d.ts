4bab3fa0866f5adda61c47182b71874d268510e6
kevinccbsg <kevinccbsg@gmail.com>
2023-12-13 23:20:56 +0100

    feat: add basic supabase config

--- globals.d.ts
+++ globals.d.ts
@@ -2,6 +2,8 @@ export {};
 declare global {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
+    SUPABASE_URL: string;
+    SUPABASE_ANON_KEY: string;
   }
   interface Process {
     env: ProcessEnv;
