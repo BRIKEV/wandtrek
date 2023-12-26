450c2d4b03fdbb63737abf6eaa28ea621c4323d0
kevinccbsg <kevinccbsg@gmail.com>
2023-12-26 23:41:11 +0100

    Refactor EditStopCard component to include a Link for editing stops

--- app/components/EditStopCard/EditStopCard.tsx
+++ app/components/EditStopCard/EditStopCard.tsx
@@ -1,22 +1,26 @@
+import { Link } from "@remix-run/react";
 import { Button } from "~/@ui/components/ui/button";
 import { Card, CardHeader } from "~/@ui/components/ui/card";
 
 interface Props {
   title: string;
   id?: string;
-  position: number;
-  removeStop: (position: number) => void;
+  order: number;
+  position: [number, number];
+  removeStop: (order: number) => void;
 }
 
-const EditStopCard = ({ title, id, removeStop, position }: Props) => (
+const EditStopCard = ({ title, id, removeStop, order, position }: Props) => (
   <Card className="mb-3">
     <CardHeader>
-      <h2>{title}</h2>
+      <h2>{order + 1}. {title}</h2>
       <div className="flex gap-x-2 items-center justify-start">
-        <Button size="icon" disabled={!id} onClick={() => removeStop(position)} className="text-xs">
-          <span translate="no" className="material-symbols-outlined">edit</span>
+        <Button size="icon" className="text-xs" asChild>
+          <Link to={`stop-edit/${id || `new?order=${order}&lt=${position[0]}&lg=${position[1]}`}`}>
+            <span translate="no" className="material-symbols-outlined">edit</span>
+          </Link>
         </Button>
-        <Button size="icon" variant="destructive" onClick={() => removeStop(position)}>
+        <Button size="icon" variant="destructive" onClick={() => removeStop(order)}>
           <span translate="no" className="material-symbols-outlined">delete</span>
         </Button>
       </div>
